e37d3b7a34f4d600b36f5ca5cd81eb80c984a987
luuishenriique <luis_henrique_co@hotmail.com>
2023-09-06 15:46:56 -0300

    Generating middlewares for token and verify authentication

---
+++ app/middlewares/verifySignUp.js
@@ -0,0 +1,57 @@
+const db = require("../models");
+const ROLES = db.ROLES;
+const User = db.user;
+
+checkDuplicateUsernameOrEmail = (req, res, next) => {
+
+  User.findOne({
+    where: {
+      username: req.body.username
+    }
+  }).then(user => {
+    if (user) {
+      res.status(400).send({
+        message: "Já existe um usuário com este nome de usuário!"
+      });
+      return;
+    }
+
+    // Email
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(user => {
+      if (user) {
+        res.status(400).send({
+          message: "Já existe um usuário com este email!"
+        });
+        return;
+      }
+
+      next();
+    });
+  });
+};
+
+checkRolesExisted = (req, res, next) => {
+  if (req.body.roles) {
+    for (let i = 0; i < req.body.roles.length; i++) {
+      if (!ROLES.includes(req.body.roles[i])) {
+        res.status(400).send({
+          message: "Tipo de usuário inexistente: " + req.body.roles[i]
+        });
+        return;
+      }
+    }
+  }
+  
+  next();
+};
+
+const verifySignUp = {
+  checkDuplicateUsernameOrEmail: checkDuplicateUsernameOrEmail,
+  checkRolesExisted: checkRolesExisted
+};
+
+module.exports = verifySignUp;
