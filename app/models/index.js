48bc58a0812cfbbe0d47c2a72ca297ed4c079ef0
luuishenriique <luis_henrique_co@hotmail.com>
2023-09-06 15:46:00 -0300

    Creating user and role models for authentication and activating in database

--- app/models/index.js
+++ app/models/index.js
@@ -19,6 +19,17 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
+db.users = require("./user.model")(sequelize, Sequelize);
+db.roles = require("./role.model.js")(sequelize, Sequelize);
 db.items = require("./item.model.js")(sequelize, Sequelize);
+
+db.roles.belongsToMany(db.users, {
+  through: "user_roles"
+});
+db.users.belongsToMany(db.roles, {
+  through: "user_roles"
+});
+
+db.ROLES = ["user", "admin", "moderator"];
 
 module.exports = db;
